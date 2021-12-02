2544ba427c26e4080f0730b7f6d5bab046699159
MaduhCrema <eduardacremacarlos2016@gmail.com>
2021-12-02 10:59:12 -0300

    new

---
+++ app/src/main/java/com/example/virtuallabel/Cadastro2.kt
@@ -0,0 +1,11 @@
+package com.example.virtuallabel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Cadastro2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cadastro2)
+    }
+}
---
+++ app/src/main/java/com/example/virtuallabel/Entrar.kt
@@ -0,0 +1,11 @@
+package com.example.virtuallabel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Entrar : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_entrar)
+    }
+}
